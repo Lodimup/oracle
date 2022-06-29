6a65148a7c3ee8194051f3fc1d14e8d9a0a8e5e3
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2022-06-29 18:58:42 +0700

    RENAME fn

--- server.py
+++ server.py
@@ -5,15 +5,15 @@ from redis.commands.json.path import Path
 app = FastAPI()
 
 @app.get("/latest")
-async def root():
+async def latest():
     # should be loaded from .env, import os, os.getenv etc...
     rdb = redis.Redis(host='localhost', port=6379, password='foobared')
     db_price = rdb.json().get('price:agg', Path.root_path())
     rdb.close()
     del db_price['ts']
-    
+
     dct_resp = {}
     for k, v in db_price.items():
         dct_resp[k] = str(v)
-    
+
     return {"prices": dct_resp}
